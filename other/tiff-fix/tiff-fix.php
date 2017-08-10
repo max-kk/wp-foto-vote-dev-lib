2f70a1ce3d8834f7c9c1d65b042bbc304a8bf468
Maxim Kaminsky <max_work@hotmail.com>
2017-08-10 10:56:07 +0300

    tiff fix

---
+++ other/tiff-fix/tiff-fix.php
@@ -0,0 +1,20 @@
+<?php
+/*
+apply_filters( 'fv/public/upload/media_handle_upload_overrides',
+    array(
+        'test_form'=>false,
+        'test_type'=>true,
+        'mimes'=> array(
+            'jpg|jpeg|jpe' => 'image/jpeg',
+            'gif' => 'image/gif',
+            'png' => 'image/png',
+        )
+), $new_photo_data)
+*/
+
+// Allow upload .tiff files
+// Requrires PHP 5.3+
+add_filter('fv/public/upload/media_handle_upload_overrides', function($args, $photo_data_arr) {
+    $args['mimes']['tiff'] = 'image/tiff';
+    return $args;
+});
