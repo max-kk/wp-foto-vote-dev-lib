b713dca9d05bb9284bc8c96ea2d1ef73d8f2db1e
Maxim Kaminsky <max_work@hotmail.com>
2018-04-19 11:34:52 +0300

    redirect after vote

--- js_public/redirect_on_close_modal.js
+++ js_public/redirect_on_close_modal.js
@@ -10,3 +10,42 @@ if ( typeof(FvLib) != "undefined" ) {
         window.location.href = "http://URL.com/blog/";
     }, 10, 1); 
 }
+
+// Or Redirect after successful Vote
+
+jQuery( document ).ready(function() {
+    if (FvLib == undefined) {
+        return;
+    }
+
+    // ## Fire some actions when wote is completed
+    FvLib.addFilter('fv/vote/get_data', function(data){
+        /**
+         "data.res" can pass : [
+         1 => 'Ok',
+         2 => 'Already voted',
+         3 => '24 hours not passed',
+         4 => 'date end',
+         5 => 'not authorized',
+         6 => 'wrong reCAPTCHA',
+         66 => 'need reCAPTCHA',
+         98 => 'invalid security token',
+         99 => 'error',
+         101 => 'need payment',
+         ]
+         */
+
+        if ( data.res == 1 ) {
+            // Add hook
+            FvLib.addHook('fv/modal/close', function(screen) {
+                // Change page URL
+                window.location.href = "http://URL.com/blog/";
+            }, 10, 1); 
+        }
+
+        /// !! REQUIRED !!
+        return data;
+    }, 10, 1);
+
+});
+
