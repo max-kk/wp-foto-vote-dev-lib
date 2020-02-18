c745e4f8957f7321b76ebbf7d47f91bf83e83093
Maxim <max_work@hotmail.com>
2020-02-18 13:19:12 +0200

    Update custom-voting-frequency.php

--- admin/custom-voting-frequency.php
+++ admin/custom-voting-frequency.php
@@ -3,6 +3,7 @@
 /* 
  Only for version 2.3.19 + 
  https://snipboard.io/gkWCvK.jpg
+ 21 second = 0.05 * 360 seconds in the hour
 */
 
 add_action('fv/admin/contest_settings/voting_frequency', function($contest) {
