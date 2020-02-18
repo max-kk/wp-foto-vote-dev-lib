899d2af7738b7b922c58f68f0a442dc1ee55d25d
Maxim <max_work@hotmail.com>
2020-02-18 13:18:16 +0200

    Create custom-voting-frequency.php

---
+++ admin/custom-voting-frequency.php
@@ -0,0 +1,10 @@
+<?php
+// COPY AFTER
+/* 
+ Only for version 2.3.19 + 
+ https://snipboard.io/gkWCvK.jpg
+*/
+
+add_action('fv/admin/contest_settings/voting_frequency', function($contest) {
+    echo '<option value="0.05h" ', selected('0.05h', $contest->voting_frequency) ,'>21 second</option>';
+});
