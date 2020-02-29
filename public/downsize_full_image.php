3ce7ac667bab09d11f40d0498d4e277bd73339eb
Maxim <max_work@hotmail.com>
2020-02-29 18:23:04 +0200

    Update downsize_full_image.php

--- public/downsize_full_image.php
+++ public/downsize_full_image.php
@@ -2,19 +2,19 @@
 // COPY after
 
 add_filter( 'fv/get_photo_full', function($url, $competitor){
-          $thumb_size = array(
-              'width'     => 1600,
-              'height'    => 0,
-              'crop'      => false,
-              'size_name' => 'fv-large',
-          );
+$thumb_size = array(
+    'width'     => 1600,
+    'height'    => 0,
+    'crop'      => false,
+    'size_name' => 'fv-large',
+);
 
-          $image_full_resized = FvFunctions::getPhotoThumbnailArr($competitor, $thumb_size);
+$image_full_resized = FvFunctions::getPhotoThumbnailArr($competitor, $thumb_size);
 
-          if ( $image_full_resized ) {
-              return $image_full_resized[0];
-          }
-
-    return $url;
-  }, 10, 2);	
+if ( $image_full_resized ) {
+    return $image_full_resized[0];
 }
+
+return $url;
+}, 10, 2);	
+
